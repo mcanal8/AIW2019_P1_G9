a072d5a846ebe705c960daf450a13dc9ad5cda1f
victor_vila <viko_bcn_@hotmail.com>
2019-01-17 16:56:41 +0100

    added files

--- src/main/java/utils/CreateTrainingDataSimple.java
+++ src/main/java/utils/CreateTrainingDataSimple.java
@@ -28,6 +28,7 @@ public class CreateTrainingDataSimple {
         /* airplane accidents spanish */
         inDir=new File(".\\resources\\Concisus-v3\\text_files\\aviation\\spanish");
         flist=inDir.listFiles();
+
         for(int f=0;f<flist.length;f++) {
             floc=flist[f].getAbsolutePath();
             try {
